267aa0480b80d8ffa204cb660c088e29a062fb20
Michael Hanke <michael.hanke@gmail.com>
2018-10-28 15:36:09 +0100

    Ready for release

--- setup.py
+++ setup.py
@@ -18,28 +18,35 @@ def get_version():
     return version_lines[0].split('=')[1].strip(" '\"\t\n")
 
 
+# PyPI doesn't render markdown yet. Workaround for a sane appearance
+# https://github.com/pypa/pypi-legacy/issues/148#issuecomment-227757822
+README = op.join(op.dirname(__file__), 'README.md')
+try:
+    import pypandoc
+    long_description = pypandoc.convert(README, 'rst')
+except (ImportError, OSError) as exc:
+    # attempting to install pandoc via brew on OSX currently hangs and
+    # pypandoc imports but throws OSError demanding pandoc
+    print(
+        "WARNING: pypandoc failed to import or thrown an error while converting"
+        " README.md to RST: %r   .md version will be used as is" % exc
+    )
+    long_description = open(README).read()
+
+
 setup(
-    # basic project properties can be set arbitrarily
     name="datalad_revolution",
     author="The DataLad Team and Contributors",
     author_email="team@datalad.org",
     version=get_version(),
     description="Revolutionary DataLad extension package",
     packages=[pkg for pkg in find_packages('.') if pkg.startswith('datalad')],
-    # datalad command suite specs from here
     install_requires=[
-        # in general datalad will be a requirement, unless the datalad extension
-        # aspect is an optional component of a larger project
-        # disable for now as we currently need a Git snapshot (requirements.txt)
         'datalad',
         'pathlib2; python_version < "3.0"',
     ],
     entry_points = {
-        # 'datalad.extensions' is THE entrypoint inspected by the datalad API builders
         'datalad.extensions': [
-            # the label in front of '=' is the command suite label
-            # the entrypoint can point to any symbol of any name, as long it is
-            # valid datalad interface specification (see demo in this extensions
             'revolution=datalad_revolution:command_suite',
         ]
     },
