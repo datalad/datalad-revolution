3c2945c5532acbb280717072d16f0400917444e7
Michael Hanke <michael.hanke@gmail.com>
2018-12-18 08:23:15 +0100

    RF: Improve modularization of diff functionality

--- datalad_revolution/revdiff.py
+++ datalad_revolution/revdiff.py
@@ -19,7 +19,6 @@ from six import (
     text_type,
 )
 from collections import OrderedDict
-from datalad.dochelpers import exc_str
 from datalad.utils import (
     assure_list,
 )
@@ -28,8 +27,12 @@ from datalad.interface.base import (
     build_doc,
 )
 from datalad.interface.utils import eval_results
+from datalad.interface.common_opts import (
+    recursion_limit,
+    recursion_flag,
+)
 
-from datalad_revolution.dataset import (
+from .dataset import (
     RevolutionDataset as Dataset,
     EnsureDataset,
     datasetmethod,
@@ -39,16 +42,58 @@ from datalad_revolution.dataset import (
     get_dataset_root,
 )
 import datalad_revolution.utils as ut
-from datalad_revolution.revstatus import (
-    RevStatus,
-)
 
-from datalad.support.constraints import EnsureNone
-from datalad.support.constraints import EnsureStr
+from datalad.support.constraints import (
+    EnsureNone,
+    EnsureStr,
+    EnsureChoice,
+)
 from datalad.support.param import Parameter
 from datalad.consts import PRE_INIT_COMMIT_SHA
 
 lgr = logging.getLogger('datalad.revolution.diff')
+
+
+_common_diffstatus_params = dict(
+    dataset=Parameter(
+        args=("-d", "--dataset"),
+        doc="""specify the dataset to query.  If
+        no dataset is given, an attempt is made to identify the dataset
+        based on the current working directory""",
+        constraints=EnsureDataset() | EnsureNone()),
+    path=Parameter(
+        args=("path",),
+        metavar="PATH",
+        doc="""path to be evaluated""",
+        nargs="*",
+        constraints=EnsureStr() | EnsureNone()),
+    annex=Parameter(
+        args=('--annex',),
+        metavar='MODE',
+        constraints=EnsureChoice(None, 'basic', 'availability', 'all'),
+        doc="""Switch whether to include information on the annex
+        content of individual files in the status report, such as
+        recorded file size. By default no annex information is reported
+        (faster). Three report modes are available: basic information
+        like file size and key name ('basic'); additionally test whether
+        file content is present in the local annex ('availability';
+        requires one or two additional file system stat calls, but does
+        not call git-annex), this will add the result properties
+        'has_content' (boolean flag) and 'objloc' (absolute path to an
+        existing annex object file); or 'all' which will report all
+        available information (presently identical to 'availability').
+        """),
+    untracked=Parameter(
+        args=('--untracked',),
+        metavar='MODE',
+        constraints=EnsureChoice('no', 'normal', 'all'),
+        doc="""If and how untracked content is reported when comparing
+        a revision to the state of the work tree. 'no': no untracked
+        content is reported; 'normal': untracked files and entire
+        untracked directories are reported as such; 'all': report
+        individual files even in fully untracked directories."""),
+    recursive=recursion_flag,
+    recursion_limit=recursion_limit)
 
 
 @build_doc
@@ -60,7 +105,7 @@ class RevDiff(Interface):
     result_renderer = 'tailored'
 
     _params_ = dict(
-        RevStatus._params_,
+        _common_diffstatus_params,
         fr=Parameter(
             args=("-f", "--from",),
             dest='fr',
@@ -100,67 +145,18 @@ class RevDiff(Interface):
         if isinstance(to, list):
             to = to[0]
 
-        # we cannot really perform any sorting of paths into subdatasets
-        # or rejecting paths based on the state of the filesystem, as
-        # we need to be able to compare with states that are not represented
-        # in the worktree (anymore)
-        if path:
-            ps = []
-            # sort any path argument into the respective subdatasets
-            for p in sorted(assure_list(path)):
-                # it is important to capture the exact form of the
-                # given path argument, before any normalization happens
-                # distinguish rsync-link syntax to identify
-                # a dataset as whole (e.g. 'ds') vs its
-                # content (e.g. 'ds/')
-                # special case is the root dataset, always report its content
-                # changes
-                orig_path = str(p)
-                resolved_path = resolve_path(p, dataset)
-                p = \
-                    resolved_path, \
-                    orig_path.endswith(op.sep) or resolved_path == ds.pathobj
-                root = get_dataset_root(str(p[0]))
-                if root is None:
-                    # no root, not possibly underneath the refds
-                    yield dict(
-                        action='status',
-                        path=str(p[0]),
-                        refds=ds.path,
-                        status='error',
-                        message='path not underneath this dataset',
-                        logger=lgr)
-                    continue
-                ps.append(p)
-            path = ps
-
-        # TODO we might want to move away from the single-pass+immediate-yield
-        # paradigm for this command. If we gather all information first, we
-        # could do post-processing and detect when a file (same gitsha, or same
-        # key) was copied/moved from another dataset. Another command (e.g.
-        # rev-save) could act on this information and also move/copy
-        # availability information or at least enhance the respective commit
-        # message with cross-dataset provenance info
-
-        # cache to help avoid duplicate status queries
-        content_info_cache = {}
-        for res in _diff_ds(
-                ds,
-                fr,
-                to,
-                recursion_limit
-                if recursion_limit is not None and recursive
-                else -1 if recursive else 0,
-                # TODO recode paths to repo path reference
-                origpaths=None if not path else OrderedDict(path),
+        for r in _diff_cmd(
+                ds=ds,
+                dataset=dataset,
+                fr=fr,
+                to=to,
+                constant_refs=False,
+                path=path,
+                annex=annex,
                 untracked=untracked,
-                cache=content_info_cache):
-            res.update(
-                refds=ds.path,
-                logger=lgr,
-                action='diff',
-            )
-            yield res
+                recursive=recursive,
+                recursion_limit=recursion_limit):
+            yield r
 
     @staticmethod
     def custom_result_renderer(res, **kwargs):  # pragma: no cover
@@ -197,7 +193,102 @@ class RevDiff(Interface):
                 ut.ac.color_word(type_, ut.ac.MAGENTA) if type_ else '')))
 
 
-def _diff_ds(ds, fr, to, recursion_level, origpaths, untracked, cache):
+def _diff_cmd(
+        ds,
+        dataset,
+        fr,
+        to,
+        constant_refs,
+        path=None,
+        annex=None,
+        untracked='normal',
+        recursive=False,
+        recursion_limit=None):
+    """Internal helper to actually run the command"""
+    # we cannot really perform any sorting of paths into subdatasets
+    # or rejecting paths based on the state of the filesystem, as
+    # we need to be able to compare with states that are not represented
+    # in the worktree (anymore)
+    if path:
+        ps = []
+        # sort any path argument into the respective subdatasets
+        for p in sorted(assure_list(path)):
+            # it is important to capture the exact form of the
+            # given path argument, before any normalization happens
+            # distinguish rsync-link syntax to identify
+            # a dataset as whole (e.g. 'ds') vs its
+            # content (e.g. 'ds/')
+            # special case is the root dataset, always report its content
+            # changes
+            orig_path = str(p)
+            resolved_path = resolve_path(p, dataset)
+            p = \
+                resolved_path, \
+                orig_path.endswith(op.sep) or resolved_path == ds.pathobj
+            str_path = text_type(p[0])
+            root = get_dataset_root(str_path)
+            if root is None:
+                # no root, not possibly underneath the refds
+                yield dict(
+                    action='status',
+                    path=str_path,
+                    refds=ds.path,
+                    status='error',
+                    message='path not underneath this dataset',
+                    logger=lgr)
+                continue
+            if path_under_dataset(ds, str_path) is None:
+                # nothing we support handling any further
+                # there is only a single refds
+                yield dict(
+                    path=str_path,
+                    refds=ds.path,
+                    action='diff',
+                    status='error',
+                    message=(
+                        "dataset containing given paths is not underneath "
+                        "the reference dataset %s: %s",
+                        ds, str_path),
+                    logger=lgr,
+                )
+                continue
+
+            ps.append(p)
+        path = ps
+
+    # TODO we might want to move away from the single-pass+immediate-yield
+    # paradigm for this command. If we gather all information first, we
+    # could do post-processing and detect when a file (same gitsha, or same
+    # key) was copied/moved from another dataset. Another command (e.g.
+    # rev-save) could act on this information and also move/copy
+    # availability information or at least enhance the respective commit
+    # message with cross-dataset provenance info
+
+    # cache to help avoid duplicate status queries
+    content_info_cache = {}
+    for res in _diff_ds(
+            ds,
+            fr,
+            to,
+            constant_refs,
+            recursion_limit
+            if recursion_limit is not None and recursive
+            else -1 if recursive else 0,
+            # TODO recode paths to repo path reference
+            origpaths=None if not path else OrderedDict(path),
+            untracked=untracked,
+            annexinfo=annex,
+            cache=content_info_cache):
+        res.update(
+            refds=ds.path,
+            logger=lgr,
+            action='diff',
+        )
+        yield res
+
+
+def _diff_ds(ds, fr, to, constant_refs, recursion_level, origpaths, untracked,
+             annexinfo, cache):
     repo_path = ds.repo.pathobj
     # filter and normalize paths that match this dataset before passing them
     # onto the low-level query method
@@ -228,6 +319,21 @@ def _diff_ds(ds, fr, to, recursion_level, origpaths, untracked, cache):
             )
             return
 
+    if annexinfo and hasattr(ds.repo, 'get_content_annexinfo'):
+        # this will ammend `status`
+        ds.repo.get_content_annexinfo(
+            paths=paths if paths else None,
+            init=diff_state,
+            eval_availability=annexinfo in ('availability', 'all'),
+            ref=to)
+        if fr != to:
+            ds.repo.get_content_annexinfo(
+                paths=paths if paths else None,
+                init=diff_state,
+                eval_availability=annexinfo in ('availability', 'all'),
+                ref=fr,
+                key_prefix="prev_")
+
     for path, props in iteritems(diff_state):
         pathinds = str(ds.pathobj / path.relative_to(repo_path))
         yield dict(
@@ -252,15 +358,20 @@ def _diff_ds(ds, fr, to, recursion_level, origpaths, untracked, cache):
                 for r in _diff_ds(
                         subds,
                         # from before time or from the reported state
-                        PRE_INIT_COMMIT_SHA
+                        fr if constant_refs
+                        else PRE_INIT_COMMIT_SHA
                         if subds_state == 'added'
                         else props['prev_gitshasum'],
                         # to the last recorded state, or the worktree
-                        None if to is None else props['gitshasum'],
+                        None if to is None
+                        else to if constant_refs
+                        else props['gitshasum'],
+                        constant_refs,
                         # subtract on level on the way down
                         recursion_level=recursion_level - 1,
                         origpaths=origpaths,
                         untracked=untracked,
+                        annexinfo=annexinfo,
                         cache=cache):
                     yield r
             else:
