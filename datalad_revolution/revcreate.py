73eb2c7a015204b3e84949a26bec114c349e930c
Kyle Meyer <kyle@kyleam.com>
2019-05-03 14:58:54 -0400

    Resurrect rev-create as shim for core's create
    datalad/datalad#3383 replaces core's create with rev-create.  Add back
    a compatibility alias, like we already do for diff and status.

--- datalad_revolution/revcreate.py
+++ datalad_revolution/revcreate.py
@@ -10,4 +10,41 @@ if '_generate_extension_api' not in _tb:  # pragma: no cover
         'The `RevCreate` class can be imported with: '
         '`from datalad.core.local.create import Create as RevCreate`')
 
-from datalad.core.local.create import Create as RevCreate
+from datalad.interface.base import (
+    build_doc,
+)
+from datalad.interface.utils import eval_results
+from .dataset import (
+    rev_datasetmethod,
+)
+
+from datalad.core.local.create import Create
+
+
+@build_doc
+class RevCreate(Create):
+
+    @staticmethod
+    @rev_datasetmethod(name='rev_create')
+    @eval_results
+    def __call__(path=None,
+                 initopts=None,
+                 force=False,
+                 description=None,
+                 dataset=None,
+                 no_annex=False,
+                 fake_dates=False,
+                 cfg_proc=None):
+        for r in Create.__call__(path=path,
+                                 initopts=initopts,
+                                 force=force,
+                                 description=description,
+                                 dataset=dataset,
+                                 no_annex=no_annex,
+                                 fake_dates=fake_dates,
+                                 cfg_proc=cfg_proc,
+                                 result_renderer=None,
+                                 result_xfm=None,
+                                 on_failure="ignore",
+                                 return_type='generator'):
+            yield r
